c7256b90b24894174c35bd2537634d853d3a28df
Colby Todd <cjhtodd11@gmail.com>
2024-05-09 23:01:51 -0400

    Calculates best possible combo

--- sample/core.py
+++ sample/core.py
@@ -7,6 +7,14 @@ COLOURS = set((1, 2, 3, 4, 5, 6))
 COMBINATIONS = set(product(COLOURS, repeat = COLUMNS))
 
 calculator = Calculator()
+best_combo = ()
+max_expected_information = 0
 
 for combo in COMBINATIONS:
-    calculator.calculate_expected_information(combo, COMBINATIONS, HINTS)
+    expected_information = calculator.calculate_expected_information(combo, COMBINATIONS, HINTS)
+    
+    if expected_information > max_expected_information:
+        best_combo = combo
+        max_expected_information = expected_information
+
+print("Best combo = " + str(combo) + "\n Expected information = " + str(max_expected_information))
