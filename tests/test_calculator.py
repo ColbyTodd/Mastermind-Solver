3908fd7cd546008c770a003a61c3785c8b77b187
Colby Todd <cjhtodd11@gmail.com>
2024-05-13 01:42:11 -0400

    Created test cases for test_common_elements_same_spot

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,4 +4,14 @@ from context import Calculator
 CALCULATOR = Calculator()
 
 def test_commmon_elements_same_spot():
+    # Basic Test
     assert(CALCULATOR.common_elements_same_spot((1, 2, 1, 1), (1, 2, 3, 1)) == 3)
+
+    # None in the same spot
+    assert(CALCULATOR.common_elements_same_spot((0, 3, 1, 2), (1, 2, 3, 1)) == 0)
+
+    # None are the same
+    assert(CALCULATOR.common_elements_same_spot((0, 0, 0, 0), (1, 1, 1, 1)) == 0)
+
+    # All are the same
+    assert(CALCULATOR.common_elements_same_spot((1, 2, 1, 1), (1, 2, 1, 1)) == 4)
