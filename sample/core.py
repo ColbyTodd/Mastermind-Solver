386223e88e9f21df807508c1ccebe5a5ab2d7b66
Colby Todd <cjhtodd11@gmail.com>
2024-05-14 02:04:40 -0400

    Refactored find_best_combo from core to Solver class

--- sample/core.py
+++ sample/core.py
@@ -1,27 +1,10 @@
 from itertools import product, combinations_with_replacement
-from Calculator import Calculator
+from Solver import Solver
 
 COLUMNS = 4
-HINTS = set(combinations_with_replacement(set((0, 1, 2)), COLUMNS))
 COLOURS = set((1, 2, 3, 4, 5, 6))
 COMBINATIONS = set(product(COLOURS, repeat = COLUMNS))
+HINTS = set(combinations_with_replacement(set((0, 1, 2)), COLUMNS))
 
-def find_best_combo():
-    calculator = Calculator()
-    colours = set()
-    for i in range (1, COLUMNS + 1):
-        colours.add(i)
-    combinations = set(product(colours, repeat = COLUMNS))
-    best_combo = ()
-    max_expected_information = 0
-
-    for combo in combinations:
-        expected_information = calculator.calculate_expected_information(combo, COMBINATIONS, HINTS)
-        
-        if expected_information > max_expected_information:
-            best_combo = combo
-            max_expected_information = expected_information
-
-    print("Best combo = " + str(best_combo) + "\nExpected information = " + str(max_expected_information))
-
-find_best_combo()
+solver = Solver(COLUMNS, COLOURS, COMBINATIONS, HINTS)
+solver.find_best_combo()
