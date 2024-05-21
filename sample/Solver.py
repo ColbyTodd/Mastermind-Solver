1ff21b2bba9e7462876222cd3c4d44dcc2ed6444
Colby Todd <cjhtodd11@gmail.com>
2024-05-21 13:57:06 -0400

    Created new function for best combo with lookahead

--- sample/Solver.py
+++ sample/Solver.py
@@ -5,10 +5,11 @@ class Solver:
 
     def __init__(self, columns: int, colours: set[int], combinations: set[tuple[int]], hints: set[tuple[int]]) -> None:
         self.columns = columns
+        self.colours = colours
         self.combinations = combinations
         self.hints = hints
     
-    def find_best_combo(self):
+    def find_best_combo(self) -> tuple[int]:
         calculator = Calculator()
         colours = set()
         for i in range (1, self.columns + 1):
@@ -25,3 +26,25 @@ class Solver:
                 max_expected_information = expected_information
 
         print("Best combo = " + str(best_combo) + "\nExpected information = " + str(max_expected_information))
+
+        return best_combo
+
+    def find_best_combo_with_lookahead(self) -> tuple[int]:
+        calculator = Calculator()
+        colours = set()
+        for i in range (1, self.columns + 1):
+            colours.add(i)
+        combinations = set(product(colours, repeat = self.columns))
+        best_combo = ()
+        max_expected_information = 0
+
+        for combo in combinations:
+            expected_information = calculator.calculate_expected_information(combo, self.combinations, self.hints)
+            
+            if expected_information > max_expected_information:
+                best_combo = combo
+                max_expected_information = expected_information
+
+        print("Best combo = " + str(best_combo) + "\nExpected information = " + str(max_expected_information))
+
+        return best_combo
