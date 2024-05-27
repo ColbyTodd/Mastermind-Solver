a3af49ac71109cbab2f7261f1ce9d0f9c9471d13
Colby Todd <cjhtodd11@gmail.com>
2024-05-27 14:45:51 -0400

    Changed back to default settings

--- sample/core.py
+++ sample/core.py
@@ -1,10 +1,10 @@
 from itertools import product, combinations_with_replacement
 from Solver import Solver
 
-COLUMNS = 5
-COLOURS = set((1, 2, 3, 4))
+COLUMNS = 4
+COLOURS = set((1, 2, 3, 4, 5, 6))
 COMBINATIONS = set(product(COLOURS, repeat = COLUMNS))
 HINTS = set(combinations_with_replacement(set((0, 1, 2)), COLUMNS))
 
 solver = Solver(COLUMNS, COLOURS, COMBINATIONS, HINTS)
-solver.find_best_combo()
+solver.find_best_combo_with_lookahead(3)
