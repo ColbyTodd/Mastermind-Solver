892d7868199199241d2c1b45cbd6f070ef3109a5
Colby Todd <cjhtodd11@gmail.com>
2024-06-10 21:57:14 -0400

    Added hint functionality

--- sample/Mastermind.py
+++ sample/Mastermind.py
@@ -1,15 +1,23 @@
-class Mastermind:
+class mastermind:
     
     def __init__(self, ans: tuple[int]):
         self.ans = ans
     
     def hint(self, guess: tuple[int]) -> tuple[int]:
-        hints = ()
+        hints = []
+        n = len(guess)
         if guess == self.ans:
             return (3)
         
-        for i in range(len(guess)):
+        for i in range(n):
             if guess[i] == self.ans[i]:
-                hints += (2)
-            
-            
+                hints.append(2)
+                guess[i] = -1
+                self.ans[i] = -1
+        
+        for i in range(n):
+            if guess[i] != -1 and guess[i] in self.ans:
+                self.ans.remove(guess[i])
+                hints.append(1)
+        
+        return hints
