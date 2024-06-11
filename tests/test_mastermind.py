3927f710a37de6c73206dfd8e3c60e5f856c5ef2
Colby Todd <cjhtodd11@gmail.com>
2024-06-10 22:12:33 -0400

    Added more tests

--- tests/test_mastermind.py
+++ tests/test_mastermind.py
@@ -1,6 +1,15 @@
 from context import mastermind
 
-MASTERMIND = mastermind((1, 1, 1, 1))
-
 def test_hint():
-    assert(MASTERMIND.hint((1, 2, 3, 1)) == (2, 2))
+    game = mastermind((1, 2, 3, 4))
+    assert(game.hint((1, 2, 3, 1)) == (2, 2, 2))
+
+    assert(game.hint((1, 2, 3, 4)) == (3))
+
+    assert(game.hint((1, 2, 5, 3)) == (2, 2, 1))
+
+    assert(game.hint((1, 1, 5, 3)) == (2, 1))
+
+    assert(game.hint((5, 5, 5, 5)) == ())
+
+    assert(game.hint((2, 3, 4, 1)) == (1, 1, 1, 1))
