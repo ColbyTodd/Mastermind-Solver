89d59e6fa7262f0f7cbfaf19b369bf4dfbd17633
Colby Todd <cjhtodd11@gmail.com>
2024-06-11 14:24:42 -0400

    Fixed test_hint

--- tests/test_mastermind.py
+++ tests/test_mastermind.py
@@ -2,14 +2,14 @@ from context import Mastermind
 
 def test_hint():
     game = Mastermind((1, 2, 3, 4))
-    assert(game.hint((1, 2, 3, 1)) == (2, 2, 2))
+    assert(game.hint((1, 2, 3, 1)) == (2, 2, 2, 0))
 
-    assert(game.hint((1, 2, 3, 4)) == (3))
+    assert(game.hint((1, 2, 3, 4)) == (2, 2, 2, 2))
 
-    assert(game.hint((1, 2, 5, 3)) == (2, 2, 1))
+    assert(game.hint((1, 2, 5, 3)) == (2, 2, 1, 0))
 
-    assert(game.hint((1, 1, 5, 3)) == (2, 1))
+    assert(game.hint((1, 1, 5, 3)) == (2, 1, 0, 0))
 
-    assert(game.hint((5, 5, 5, 5)) == ())
+    assert(game.hint((5, 5, 5, 5)) == (0, 0, 0, 0))
 
     assert(game.hint((2, 3, 4, 1)) == (1, 1, 1, 1))
